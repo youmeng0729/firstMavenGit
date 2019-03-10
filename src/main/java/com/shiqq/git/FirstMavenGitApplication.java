93c8ce446f71c8a7d6d4571dc5aff7a549012344
youme <youme@windows10.microdone.cn>
2019-03-10 16:15:16 +0800

    My first Commit

---
+++ src/main/java/com/shiqq/git/FirstMavenGitApplication.java
@@ -0,0 +1,13 @@
+package com.shiqq.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMavenGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMavenGitApplication.class, args);
+	}
+
+}
